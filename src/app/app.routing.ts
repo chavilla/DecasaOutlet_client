4aca886325935e7391091d760249ad2b073f99d0
chavilla <jcharris.villa@gmail.com>
2020-11-16 14:14:35 -0500

    Se inicia el router y el loginComponent

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes  } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes:Routes=[
+    { path:'login', component: LoginComponent },
+    { path:'home', component:HomeComponent }
+];
+
+export const appRoutingProviders:any[]=[];
+export const routing:ModuleWithProviders<any>=RouterModule.forRoot(appRoutes);
