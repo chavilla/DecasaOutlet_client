6168f0a86cbf37c89e42977b44fecc2248a5066a
chavilla <jcharris.villa@gmail.com>
2021-02-15 21:50:31 -0500

    Angular Material is installed and setted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductStoreComponent } from './components/products/product-store/product-store.component';
 import { ProductAddFrameComponent } from './components/products/product-add-frame/product-add-frame.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [
     appRoutingProviders
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes  } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -6,14 +6,17 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { ProductStoreComponent } from './components/products/product-store/product-store.component';
 import { ProductAddFrameComponent } from './components/products/product-add-frame/product-add-frame.component';
 
-const appRoutes:Routes=[
-    { path:'login', component: LoginComponent },
-    { path:'home', component:HomeComponent },
-    { path:'product', component: ProductStoreComponent },
-    { path: 'product/add', component: ProductAddFrameComponent },
-    { path: '', redirectTo: '/home', pathMatch:'full' },
-    { path:'**', component: PageNotFoundComponent },
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'app', component: HomeComponent, children: [
+            { path: 'product', component: ProductStoreComponent },
+            { path: 'product/add', component: ProductAddFrameComponent },
+        ]
+    },
+    { path: '', redirectTo: '/app', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent },
 ];
 
-export const appRoutingProviders:any[]=[];
-export const routing:ModuleWithProviders<any>=RouterModule.forRoot(appRoutes);
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
